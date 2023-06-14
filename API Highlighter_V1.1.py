0f639912adb1a0d2e422a43c33e1c72dd17a2dca
Flechzao <66863063+Flechzao@users.noreply.github.com>
2023-06-14 10:46:25 +0800

    Update and rename API Highlighter_V1.0.py to API Highlighter_V1.1.py

--- API Highlighter_V1.1.py
+++ API Highlighter_V1.1.py
@@ -1,7 +1,9 @@
 from burp import IBurpExtender, IProxyListener, ITab
-from javax.swing import JPanel, JTextArea, JScrollPane, JTable, JButton, BorderFactory, BoxLayout, SwingConstants
-from javax.swing.table import DefaultTableModel, DefaultTableCellRenderer
-
+from java.lang import Boolean
+from javax.swing import JPanel, JTextArea, JScrollPane, JTable, JButton, BorderFactory, BoxLayout, SwingConstants, JCheckBox
+from javax.swing.table import DefaultTableModel, DefaultTableCellRenderer, TableCellEditor
+from java.awt import Component, GridLayout
+from java.awt.event import ActionListener
 
 info='''author:flechazo\nhttps://github.com/Flechzao/API-Highlighter'''
 print(info)
@@ -9,14 +11,43 @@ print(info)
 
 class CustomTableModel(DefaultTableModel):
     def getColumnClass(self, columnIndex):
+        if columnIndex == 3:
+            return Boolean.TYPE
         return str
-
 
 class CenterRenderer(DefaultTableCellRenderer):
     def __init__(self):
         super(CenterRenderer, self).__init__()
         self.setHorizontalAlignment(SwingConstants.CENTER)
 
+class CheckBoxRenderer(DefaultTableCellRenderer):
+    def __init__(self):
+        super(CheckBoxRenderer, self).__init__()
+
+    def getTableCellRendererComponent(self, table, value, isSelected, hasFocus, row, column):
+        checkbox = JCheckBox("", value)
+        checkbox.setHorizontalAlignment(SwingConstants.CENTER)
+        return checkbox
+
+class CheckBoxEditor(TableCellEditor, ActionListener):
+    def __init__(self):
+        self.checkbox = JCheckBox()
+        self.checkbox.setHorizontalAlignment(SwingConstants.CENTER)
+
+    def getTableCellEditorComponent(self, table, value, isSelected, row, column):
+        self.checkbox.selected = value
+        self.checkbox.addActionListener(self)
+        return self.checkbox
+
+    def getCellEditorValue(self):
+        return self.checkbox.isSelected()
+
+    def stopCellEditing(self):
+        self.fireEditingStopped()
+        return True
+
+    def actionPerformed(self, event):
+        self.fireEditingStopped()
 
 class BurpExtender(IBurpExtender, IProxyListener, ITab):
     def registerExtenderCallbacks(self, callbacks):
@@ -37,15 +68,27 @@ class BurpExtender(IBurpExtender, IProxyListener, ITab):
         self._main_panel.add(self._import_button)
         self._table_model = CustomTableModel(["Method", "API", "Comment", "TEST"], 0)
         self._table = JTable(self._table_model)
+        self._table.setRowSelectionAllowed(True)
+        self._table.setColumnSelectionAllowed(False)
         column_model = self._table.getColumnModel()
-        center_renderer = CenterRenderer()
         column_model.getColumn(0).setMaxWidth(90)
-        column_model.getColumn(3).setCellRenderer(center_renderer)
         column_model.getColumn(3).setMaxWidth(75)
+        center_renderer = CenterRenderer()
+        checkbox_renderer = CheckBoxRenderer()
+        checkbox_editor = CheckBoxEditor()
+        column_model.getColumn(3).setCellRenderer(checkbox_renderer)
+        column_model.getColumn(3).setCellEditor(checkbox_editor)
         self._table.setRowHeight(25)
         self._main_panel.add(JScrollPane(self._table))
+        self._button_panel = JPanel()
+        self._button_panel.layout = GridLayout(1, 4)
+        self._main_panel.add(self._button_panel)
         self._remove_button = JButton("Remove API", actionPerformed=self.remove_api)
-        self._main_panel.add(self._remove_button)
+        self._button_panel.add(self._remove_button)
+        self._toggle_tested_button = JButton("Toggle Tested", actionPerformed=self.toggle_tested)
+        self._button_panel.add(self._toggle_tested_button)
+        self._move_tested_to_top_button = JButton("Move Tested to Top", actionPerformed=self.move_tested_to_top)
+        self._button_panel.add(self._move_tested_to_top_button)
 
     def getUiComponent(self):
         return self._main_panel
@@ -55,20 +98,19 @@ class BurpExtender(IBurpExtender, IProxyListener, ITab):
 
     def import_apis(self, event):
         api_lines = self._text_area.text.splitlines()
-        http_methods = ["POST", "GET", "DELETE", "PUT", "post", "get", "delete", "put"]
-
+        http_methods = ["POST", "GET", "DELETE", "PUT"]
         for api_line in api_lines:
             method = None
             api = None
             for http_method in http_methods:
                 if api_line.upper().startswith(http_method):
-                    method = api_line[:len(http_method)].strip()
+                    method = http_method
                     api = api_line[len(http_method):].strip()
                     break
             if not api:
                 api = api_line.strip()
             if not self.api_exists(api):
-                self._table_model.addRow([method, api, "", "n"])
+                self._table_model.addRow([method, api, "", False])
 
     def api_exists(self, api):
         for row in range(self._table_model.getRowCount()):
@@ -77,9 +119,22 @@ class BurpExtender(IBurpExtender, IProxyListener, ITab):
         return False
 
     def remove_api(self, event):
-        selected_row = self._table.getSelectedRow()
-        if selected_row != -1:
-            self._table_model.removeRow(selected_row)
+        selected_rows = self._table.getSelectedRows()
+        for row in reversed(selected_rows):
+            self._table_model.removeRow(row)
+
+    def toggle_tested(self, event):
+        selected_rows = self._table.getSelectedRows()
+        for row in selected_rows:
+            current_value = self._table_model.getValueAt(row, 3)
+            self._table_model.setValueAt(not current_value, row, 3)
+
+    def move_tested_to_top(self, event):
+        tested_rows_indices = [index for index in range(self._table_model.getRowCount()) if self._table_model.getValueAt(index, 3)]
+        for index in reversed(tested_rows_indices):
+            tested_row = self._table_model.getDataVector().elementAt(index)
+            self._table_model.removeRow(index)
+            self._table_model.insertRow(0, tested_row)
 
     def processProxyMessage(self, messageIsRequest, message):
         if not messageIsRequest:
@@ -93,10 +148,9 @@ class BurpExtender(IBurpExtender, IProxyListener, ITab):
                 tested = self._table_model.getValueAt(row, 3)
                 comment = self._table_model.getValueAt(row, 2)
                 if api in url:
-                    message_info.setHighlight("green" if tested.lower() == "y" else "yellow")
+                    message_info.setHighlight("green" if tested else "yellow")
                     message_info.setComment(comment if comment else "Under Test")
                     break
-
 
 def create_instance():
     return BurpExtender()
